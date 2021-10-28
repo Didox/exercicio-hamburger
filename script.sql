39370ab49b653b34ec1ef2fa58e9bf4dc1152516
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-10-28 20:54:27 -0300

    SQL dos dados do banco de clientes

--- script.sql
+++ script.sql
@@ -1,15 +1,15 @@
-CREATE TABLE public.clientes (
+create database pedido_hamburger;
+
+CREATE TABLE clientes (
 	codigo int2 NULL,
 	nome varchar(255) NULL,
 	endereco varchar(255) NULL,
 	telefone varchar(20) NULL
 );
 
-
-SELECT *
-FROM clientes;
-
-
 INSERT INTO clientes
 (codigo, nome, endereco, telefone)
 VALUES(2, 'Jaqueline', 'Rua 1234', '(11)98999-9999');
+
+SELECT *
+FROM clientes;
